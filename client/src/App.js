918205da36f892754875840ec582bf38d4d813c4
Laurel Thorburn <laurelthorburn@gmail.com>
2022-02-10 18:49:01 -0700

    Changed App from StoreProvider to Provider

--- client/src/App.js
+++ client/src/App.js
@@ -16,7 +16,7 @@ import NoMatch from './pages/NoMatch';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Nav from './components/Nav';
-import { StoreProvider } from './utils/GlobalState';
+// import { StoreProvider } from './utils/GlobalState';
 import Success from './pages/Success';
 import OrderHistory from './pages/OrderHistory';
 
@@ -44,7 +44,7 @@ function App() {
     <ApolloProvider client={client}>
       <Router>
         <div>
-          <StoreProvider>
+          <Provider>
             <Nav />
             <Switch>
               <Route exact path="/" component={Home} />
@@ -55,7 +55,7 @@ function App() {
               <Route exact path="/products/:id" component={Detail} />
               <Route component={NoMatch} />
             </Switch>
-          </StoreProvider>
+          </Provider>
         </div>
       </Router>
     </ApolloProvider>
