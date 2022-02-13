d189d0242a75754de3f1c93f726cf343e1a31386
Laurel Thorburn <laurelthorburn@gmail.com>
2022-02-12 19:11:12 -0700

    Removal Storage path from pages details

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -3,7 +3,6 @@ import { Link, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 
 import Cart from '../components/Cart';
-import { useStoreContext } from '../utils/GlobalState';
 import {
   REMOVE_FROM_CART,
   UPDATE_CART_QUANTITY,
@@ -13,6 +12,8 @@ import {
 import { QUERY_PRODUCTS } from '../utils/queries';
 import { idbPromise } from '../utils/helpers';
 import spinner from '../assets/spinner.gif';
+
+import { useSelector, useDispatch } from "react-redux";
 
 function Detail() {
   const [state, dispatch] = useStoreContext();
